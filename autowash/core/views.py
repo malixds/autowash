a8420f1729c81eac9f4eabb8b53af5a7678e4128
malixds <masik2003_410@mail.ru>
2023-09-28 17:53:06 +0300

    The ready project auotwash

--- autowash/core/views.py
+++ autowash/core/views.py
@@ -2,26 +2,80 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def main(request):
 
     return render(request, 'core/main.html')
+
 
 def collapse(request):
 
     return render(request, 'core/collapse.html')
 
+
 def fitting(request):
 
     return render(request, 'core/fitting_list.html')
+
 
 def locksmith(request):
 
     return render(request, 'core/locksmith.html')
 
+
 def allServices(request):
 
     return render(request, 'core/allservices.html')
 
+
 def wash(request):
 
     return render(request, 'core/wash.html')
+
+
+def expressWash(request):
+
+    return render(request, 'core/expresswash.html')
+
+
+def contacts(request):
+
+    return render(request, 'core/contacts.html')
+
+
+def shop(request):
+
+    return render(request, 'core/shop.html')
+
+
+def euro(request):
+
+    return render(request, 'core/euro.html')\
+
+
+
+def nano(request):
+
+    return render(request, 'core/nano.html')
+
+
+def engine(request):
+
+    return render(request, 'core/engine.html')
+
+
+def interior(request):
+
+    return render(request, 'core/interior.html')
+
+def cons(request):
+
+    return render(request, 'core/cons.html')
+
+def complexClear(request):
+
+    return render(request, 'core/complexclear.html')
+
+def diskClear(request):
+
+    return render(request, 'core/diskclear.html')
